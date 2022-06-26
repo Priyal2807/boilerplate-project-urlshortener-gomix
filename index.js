245936ce9ada86ac5aa4e6e66cc7dabbf62ab6e0
Priyal2807 <priyalsobti@gmail.com>
2022-06-26 12:11:26 +0530

    changing index.js file

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns')
+const bodyParser = require("body-parser")
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+let key = 1;
+let urlObj = {}
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended:false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -15,9 +17,33 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+    res.json({ greeting: 'hello API' });
 });
+app.post("/api/shorturl", (req, res) => {
+    const { hostname } = new URL(req.body.url);
+    //console.log(hostname);
+
+   let result = dns.lookup(hostname, (err, add, fam) => {
+        if (err !== null && err.code === 'ENOTFOUND' ) {
+            res.send({ error: 'invalid url' })
+            //console.log(err);
+        }
+    })
+    if (result !== null) {
+        if (!Object.values(urlObj).includes(req.body.url)) {
+            urlObj[key] = req.body.url;
+            let a = key;
+            key++;
+            res.send({ original_url: req.body.url, short_url: a })
+        }
+    }
+    
+})
+app.get("/api/shorturl/:id", (req, res) => {
+    let url = urlObj[req.params.id];
+    return res.redirect(url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
