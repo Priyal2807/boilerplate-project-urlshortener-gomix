19d39eb1dc345ea7be352a834ff40c20777d0c9d
Priyal2807 <priyalsobti@gmail.com>
2022-06-26 12:20:33 +0530

    updating index.js

--- index.js
+++ index.js
@@ -25,18 +25,17 @@ app.post("/api/shorturl", (req, res) => {
     //console.log(hostname);
 
    let result = dns.lookup(hostname, (err, add, fam) => {
-        if (err !== null && err.code === 'ENOTFOUND' ) {
-            res.send({ error: 'invalid url' })
-            //console.log(err);
-        }
-    })
-    if (result !== null) {
+   })
+    if (Object.values(result).length !== 0) {
         if (!Object.values(urlObj).includes(req.body.url)) {
             urlObj[key] = req.body.url;
             let a = key;
             key++;
             res.send({ original_url: req.body.url, short_url: a })
         }
+    }
+    else {
+        res.send({error:'invalid url'})
     }
     
 })
