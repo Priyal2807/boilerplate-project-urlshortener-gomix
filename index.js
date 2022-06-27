f05ae7018227830b74c61423af7e62f21f96a277
Priyal2807 <priyalsobti@gmail.com>
2022-06-27 18:44:32 +0530

    adding the changed index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const dns = require('dns')
 const bodyParser = require("body-parser")
 // Basic Configuration
 const port = process.env.PORT || 3000;
-let key = 1;
-let urlObj = {}
+
+//autoIncrement.initialize(connection);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
+let urlObj = {};
+let key = 0;
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -21,40 +24,25 @@ app.get('/api/hello', function (req, res) {
     res.json({ greeting: 'hello API' });
 });
 app.post("/api/shorturl", (req, res) => {
-/*    const { hostname } = new URL(req.body.url);
-    //console.log(hostname);
 
-   let result = dns.lookup(hostname, (err, add, fam) => {
-   })*/
     let reg = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|^www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     let url = new URL(req.body.url);
-    if (!reg.test(req.body.url)) {
-        return res.json({ error: 'invalid url' })
+   
+    let result = dns.lookup(url.hostname, (e, family) => {
+        //console.log(result.hostname);
+    })
+    if (result.hostname !== undefined) {
+        key++;
+        urlObj[key] = url;
+        return res.send({ original_url: url, short_url: key });
     }
 
-    dns.lookup(url.hostname, async (e) => {
-        if (!e) {
-            urlObj[key] = req.body.url;
-            let a = key;
-            key++;
-            res.send({ original_url: req.body.url, short_url: a })
+        if (!reg.test(url)) {
+            return res.send({ error: 'invalid url' });
         }
-        
-    })
-/*    if (reg.test(req.body.url)){
-        if (!Object.values(urlObj).includes(req.body.url)) {
-            urlObj[key] = req.body.url;
-            let a = key;
-            key++;
-           return res.send({ original_url: req.body.url, short_url: a })
-        }
-        else {
-            let k = Object.keys(urlObj).find(key => urlObj[key] === req.body.url)
-            return res.send({ original_url: req.body.url, short_url: k })
-        }*/
-    //}
- 
     
+    
+
 })
 app.get("/api/shorturl/:id", (req, res) => {
     let url = urlObj[req.params.id];
